46a13924a70d269f1ff6c393e0da0037bbf1c47e
Brian <mbrianp05@gmail.com>
2025-05-24 12:06:33 -0400

    Add passcode verification

---
+++ src/auth/Authentication.java
@@ -0,0 +1,24 @@
+package auth;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Authentication {
+	public static boolean validateAccessCode(String code) {
+		boolean granted = false;
+		
+		try {
+			File srcFile = new File("passcode.txt");
+			Scanner reader = new Scanner(srcFile);
+		
+			String line = reader.nextLine();
+			
+			granted = line.equals(code);
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("No se pudo encontrar la fuente del código de acceso");
+		}
+		
+		return granted;
+	}
+}
--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -9,9 +9,15 @@ public class Validation {
 	}
 	
 	public static boolean validISSN(String str) {
-		 Pattern pattern = Pattern.compile("^ISSN [0-9]{4}-(([0-9]{4})|([0-9]{3}X))$");
-		 Matcher matcher = pattern.matcher(str);
-		 
-		 return matcher.find();
+		boolean valid = false;
+		
+		if (str.trim().length() == 14) {			
+			Pattern pattern = Pattern.compile("^ISSN [0-9]{4}-(([0-9]{4})|([0-9]{3}X))$");
+			Matcher matcher = pattern.matcher(str.trim());
+			
+			valid = matcher.find();			
+		}
+		
+		return valid;
 	}
 }
