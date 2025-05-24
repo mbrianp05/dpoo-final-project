0d645ebe6394540448b6dcadfc3db8448f757603
Brian <mbrianp05@gmail.com>
2025-05-24 11:53:15 -0400

    Fix ISSN validation

---
+++ src/Testing.java
@@ -0,0 +1,7 @@
+import schooling.Chapter;
+
+public class Testing {
+	public static void main(String[] args) {
+		//		Chapter ch = new Chapter("Cells", new String[]{"Brian"}, new String[]{""}, "Nature", "ISSN 0302-0000", "Natural Selection", 1);
+	}
+}
--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -9,7 +9,7 @@ public class Validation {
 	}
 	
 	public static boolean validISSN(String str) {
-		 Pattern pattern = Pattern.compile("ISSN [0-9]{4}-(([0-9]{4})|([0-9]{3}X))");
+		 Pattern pattern = Pattern.compile("^ISSN [0-9]{4}-(([0-9]{4})|([0-9]{3}X))$");
 		 Matcher matcher = pattern.matcher(str);
 		 
 		 return matcher.find();
