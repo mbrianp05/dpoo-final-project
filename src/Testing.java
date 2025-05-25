8fdd3491cc6ed9f19089fab833e9e044fcc9141d
Brian <mbrianp05@gmail.com>
2025-05-24 20:55:31 -0400

    Leave test constructor out

--- src/Testing.java
+++ src/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// Chapter ch = new Chapter("Cells", new String[]{"Brian"}, new String[]{""},
 		// "Nature", "ISSN 0302-0000", "Natural Selection", 1);
-		boolean granted = Authentication.validateAccessCode("XABA");
+		boolean granted = Authentication.authorize("XABA");
 
 		if (granted) {
 			System.out.println("Estas autenticado");
