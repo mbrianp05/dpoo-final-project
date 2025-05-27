1c9f124b1e92e42e46051dd7ac4f17c0a37d44da
Brian <mbrianp05@gmail.com>
2025-05-27 17:30:39 -0400

    Add profesors marks in their courses

--- src/schooling/Matriculation.java
+++ src/schooling/Matriculation.java
@@ -3,12 +3,27 @@ package schooling;
 public class Matriculation {
     private Profesor profesor;
     private PostgraduateCourse course;
+    private int mark = -1;
 
     public Matriculation(Profesor profesor, PostgraduateCourse course) {
         setProfesor(profesor);
         setCourse(course);
     }
 
+    public void setMark(int mark) {
+    	if (mark > 5 || mark < 2) throw new IllegalArgumentException("La nota válida está entre 2 y 5");
+    	
+    	this.mark = mark;
+    }
+    
+    public int getMark() {
+    	return mark;
+    }
+    
+    public boolean hasMark() {
+    	return mark != -1;
+    }
+    
     public void setProfesor(Profesor profesor) {
         if (profesor.getDegree() != null)
             throw new IllegalArgumentException(
