a13c05366ccfe2aaf557a15a4a5df68dc50338ef
Brian <mbrianp05@gmail.com>
2025-06-01 15:54:35 -0400

    Improve authentication

--- src/auth/Authentication.java
+++ src/auth/Authentication.java
@@ -5,34 +5,29 @@ import java.util.Scanner;
 
 public class Authentication {
 	private static boolean hasAccess = false;
-	private static String accessCode;
 
-	private static String fetchAccessCode() {
-		if (accessCode == null) {
-			try {
-				File srcFile = new File("passcode.txt");
-				Scanner reader = new Scanner(srcFile);
+	private static boolean validateAccessCode(String username, String password) {
+		boolean correctCredentials = false;
+		
+		try {
+			File srcFile = new File("passcode.txt");
+			Scanner reader = new Scanner(srcFile);
 
-				accessCode = reader.nextLine();
-				reader.close();
-			} catch (Exception e) {
-				System.out.println("No se pudo encontrar la fuente del c�digo de acceso");
-			}
+			String realUsername = reader.nextLine().trim();
+			String realPassword = reader.nextLine().trim();
+			
+			correctCredentials = realUsername.equals(username) && realPassword.equals(password);
+			
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("No se pudo encontrar la fuente del c�digo de acceso");
 		}
-
-		return accessCode;
+	
+		return correctCredentials;
 	}
 
-	public static int accessCodeLength() {
-		return fetchAccessCode().length();
-	}
-
-	private static boolean validateAccessCode(String code) {
-		return fetchAccessCode().equals(code);
-	}
-
-	public static boolean authorize(String passcode) {
-		hasAccess = validateAccessCode(passcode);
+	public static boolean authorize(String username, String password) {
+		hasAccess = validateAccessCode(username, password);
 
 		return hasAccess;
 	}
--- src/gui/AuthenticationPanel.java
+++ src/gui/AuthenticationPanel.java
@@ -1,6 +1,5 @@
 package gui;
 
-import gui.component.LengthSecretInput;
 import gui.event.OnAuthenticate;
 
 import javax.swing.JPanel;
@@ -15,10 +14,14 @@ import java.awt.Color;
 
 import auth.Authentication;
 
-import javax.swing.JProgressBar;
-
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AuthenticationPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -26,41 +29,85 @@ public class AuthenticationPanel extends JPanel {
 	private JLabel lblAuthorization;
 	private JLabel lblIngresaElCdigo;
 	private JLabel errorMsg;
-	private LengthSecretInput passcode;
-	private JProgressBar progressBar;
+	private JPasswordField passcode;
 	private OnAuthenticate onAuthenticateEvent;
+	private JLabel lblContrasea;
+	private JTextField textFieldUsername;
+	private JButton btnAcceder;
 
 	public AuthenticationPanel() {
 		setBackground(Color.WHITE);
-		setBorder(new LineBorder(new Color(204, 0, 102), 2, true));
-		setLayout(null);
-		add(getLblAuthorization());
-		add(getLblIngresaElCdigo());
-		add(getErrorMsg());
-		add(getPasscode());
-		add(getProgressBar());
+		setBorder(new LineBorder(new Color(255, 0, 51), 2, true));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 400, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 30, 40, 0, 0, 40, 0, 30, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		GridBagConstraints gbc_lblAuthorization = new GridBagConstraints();
+		gbc_lblAuthorization.fill = GridBagConstraints.BOTH;
+		gbc_lblAuthorization.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAuthorization.gridx = 1;
+		gbc_lblAuthorization.gridy = 1;
+		add(getLblAuthorization(), gbc_lblAuthorization);
+		GridBagConstraints gbc_lblIngresaElCdigo = new GridBagConstraints();
+		gbc_lblIngresaElCdigo.fill = GridBagConstraints.BOTH;
+		gbc_lblIngresaElCdigo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIngresaElCdigo.gridx = 1;
+		gbc_lblIngresaElCdigo.gridy = 3;
+		add(getLblIngresaElCdigo(), gbc_lblIngresaElCdigo);
+		GridBagConstraints gbc_textFieldUsername = new GridBagConstraints();
+		gbc_textFieldUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_textFieldUsername.fill = GridBagConstraints.BOTH;
+		gbc_textFieldUsername.gridx = 1;
+		gbc_textFieldUsername.gridy = 4;
+		add(getTextFieldUsername(), gbc_textFieldUsername);
+		GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
+		gbc_lblContrasea.fill = GridBagConstraints.BOTH;
+		gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
+		gbc_lblContrasea.gridx = 1;
+		gbc_lblContrasea.gridy = 6;
+		add(getLblContrasea(), gbc_lblContrasea);
+		GridBagConstraints gbc_passcode = new GridBagConstraints();
+		gbc_passcode.insets = new Insets(0, 0, 5, 5);
+		gbc_passcode.fill = GridBagConstraints.BOTH;
+		gbc_passcode.gridx = 1;
+		gbc_passcode.gridy = 7;
+		add(getPasscode(), gbc_passcode);
+		GridBagConstraints gbc_errorMsg = new GridBagConstraints();
+		gbc_errorMsg.fill = GridBagConstraints.BOTH;
+		gbc_errorMsg.insets = new Insets(0, 0, 5, 5);
+		gbc_errorMsg.gridx = 1;
+		gbc_errorMsg.gridy = 9;
+		add(getErrorMsg(), gbc_errorMsg);
+		GridBagConstraints gbc_btnAcceder = new GridBagConstraints();
+		gbc_btnAcceder.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnAcceder.fill = GridBagConstraints.VERTICAL;
+		gbc_btnAcceder.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAcceder.gridx = 1;
+		gbc_btnAcceder.gridy = 11;
+		add(getBtnAcceder(), gbc_btnAcceder);
 	}
 
 	public void listenTo(OnAuthenticate listener) {
 		onAuthenticateEvent = listener;
 	}
 	
-	private void verifyPasscode() {
-		if (Authentication.authorize(passcode.getText())) {
-			onAuthenticateEvent.granted();
+	private void verifyCredentials() {
+		if (Authentication.authorize(textFieldUsername.getText(), String.valueOf(passcode.getPassword()))) {
+			if (onAuthenticateEvent != null)
+				onAuthenticateEvent.granted();
 		} else {
 			passcode.setText("");
 			errorMsg.setVisible(true);
-			progressBar.setValue(0);
 		}
 	}
 	
 	private JLabel getLblAuthorization() {
 		if (lblAuthorization == null) {
 			lblAuthorization = new JLabel("Autorizaci\u00F3n");
-			lblAuthorization.setBounds(27, 13, 199, 32);
 			lblAuthorization.setHorizontalAlignment(SwingConstants.LEFT);
-			lblAuthorization.setFont(new Font("Segoe UI Symbol", Font.BOLD, 23));
+			lblAuthorization.setFont(new Font("Segoe UI", Font.PLAIN, 25));
 		}
 
 		return lblAuthorization;
@@ -68,9 +115,8 @@ public class AuthenticationPanel extends JPanel {
 
 	private JLabel getLblIngresaElCdigo() {
 		if (lblIngresaElCdigo == null) {
-			lblIngresaElCdigo = new JLabel("Ingresa el c\u00F3digo de acceso");
-			lblIngresaElCdigo.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 13));
-			lblIngresaElCdigo.setBounds(27, 45, 188, 32);
+			lblIngresaElCdigo = new JLabel("Nombre de usuario");
+			lblIngresaElCdigo.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 			lblIngresaElCdigo.setHorizontalAlignment(SwingConstants.LEFT);
 		}
 
@@ -79,43 +125,48 @@ public class AuthenticationPanel extends JPanel {
 
 	private JLabel getErrorMsg() {
 		if (errorMsg == null) {
-			errorMsg = new JLabel("El código no es correcto");
+			errorMsg = new JLabel("Credenciales no válidas");
 			errorMsg.setBackground(Color.WHITE);
-			errorMsg.setFont(new Font("Segoe UI Symbol", Font.BOLD, 13));
+			errorMsg.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 			errorMsg.setForeground(new Color(204, 102, 102));
-			errorMsg.setBounds(27, 80, 215, 16);
 			errorMsg.setVisible(false);
 		}
 
 		return errorMsg;
 	}
 	
-	private LengthSecretInput getPasscode() {
+	private JPasswordField getPasscode() {
 		if (passcode == null) {
-			passcode = new LengthSecretInput();
-			passcode.setMaxLength(Authentication.accessCodeLength());
-			passcode.addKeyListener(new KeyAdapter() {
-				@Override
-				public void keyReleased(KeyEvent event) {
-					progressBar.setValue(passcode.getText().length());
-					
-					if (passcode.getText().length() == passcode.getMaxLength()) {
-						
-						verifyPasscode();
-					}
-				}
-			});
-			passcode.setBounds(27, 109, 215, 32);
+			passcode = new JPasswordField();
 		}
 		return passcode;
 	}
-	private JProgressBar getProgressBar() {
-		if (progressBar == null) {
-			progressBar = new JProgressBar();
-			progressBar.setBackground(Color.WHITE);
-			progressBar.setBounds(27, 154, 215, 14);
-			progressBar.setMaximum(Authentication.accessCodeLength());
+	private JLabel getLblContrasea() {
+		if (lblContrasea == null) {
+			lblContrasea = new JLabel("Contraseña");
+			lblContrasea.setHorizontalAlignment(SwingConstants.LEFT);
+			lblContrasea.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 		}
-		return progressBar;
+		return lblContrasea;
+	}
+	private JTextField getTextFieldUsername() {
+		if (textFieldUsername == null) {
+			textFieldUsername = new JTextField();
+			textFieldUsername.setColumns(10);
+		}
+		return textFieldUsername;
+	}
+	private JButton getBtnAcceder() {
+		if (btnAcceder == null) {
+			btnAcceder = new JButton("Acceder");
+			btnAcceder.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					verifyCredentials();
+				}
+			});
+			btnAcceder.setBackground(Color.WHITE);
+			btnAcceder.setHorizontalAlignment(SwingConstants.LEFT);
+		}
+		return btnAcceder;
 	}
 }
