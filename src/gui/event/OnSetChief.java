d861530eb926b3c02ef596a67804a1adaac215bc
Brian <mbrianp05@gmail.com>
2025-06-07 12:26:18 -0400

    OnSetChief event now accepts Form Data as a type

--- src/gui/event/OnSetChief.java
+++ src/gui/event/OnSetChief.java
@@ -1,8 +1,6 @@
 package gui.event;
 
 import gui.researchers.ProfesorFormData;
-import schooling.Degree;
-import schooling.ProfesorCategory;
 
 public interface OnSetChief {
 	void set(ProfesorFormData data);
