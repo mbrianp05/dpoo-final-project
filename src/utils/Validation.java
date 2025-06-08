96c23559157d4127aaa34a52489195099a06bbc9
Brian <mbrianp05@gmail.com>
2025-06-08 10:34:48 -0400

    Correct ISSN validation

--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -51,7 +51,9 @@ public class Validation {
 		boolean valid = unique("ISSN", str);
 
 		if (valid) {
-			if (str.trim().length() == 14) {
+			valid = str.trim().length() == 14;
+			
+			if (valid) {
 				Pattern pattern = Pattern.compile("^ISSN [0-9]{4}-(([0-9]{4})|([0-9]{3}X))$");
 				Matcher matcher = pattern.matcher(str.trim());
 				
