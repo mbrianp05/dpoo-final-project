93cc290606e6d00fddd7c8801755f3eb0557e806
Brian <mbrianp05@gmail.com>
2025-06-09 20:21:16 -0400

    Small changes

--- src/gui/reasearchline/AddChiefJDialog.java
+++ src/gui/reasearchline/AddChiefJDialog.java
@@ -9,7 +9,14 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
+import javax.swing.JButton;
+
+import utils.Icons;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddChiefJDialog extends JDialog {
 	private static final long serialVersionUID = 3142798032279882910L;
@@ -18,6 +25,7 @@ public class AddChiefJDialog extends JDialog {
 	private ProfesorForm profesorForm;
 	private OnSetChief listener;
 	private ProfesorFormData data;
+	private JButton btnClose;
 	
 	/**
 	 * @wbp.parser.constructor
@@ -29,18 +37,25 @@ public class AddChiefJDialog extends JDialog {
 	public AddChiefJDialog(String[] matters, OnSetChief listener, ProfesorFormData data) {
 		setResizable(false);
 		setLocationRelativeTo(null);
+		
+		setSize(626, 200);
+		
 		this.matters = matters;
 		this.listener = listener;
 		this.data = data;
 		setTitle("Insertar datos el jefe de la l\u00EDnea");
 		setModal(true);
-		setBounds(100, 100, 626, 670);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{70, 0, 70, 0};
 		gridBagLayout.rowHeights = new int[]{70, 545, 70, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
+		GridBagConstraints gbc_btnClose = new GridBagConstraints();
+		gbc_btnClose.insets = new Insets(0, 0, 5, 0);
+		gbc_btnClose.gridx = 2;
+		gbc_btnClose.gridy = 0;
+		getContentPane().add(getBtnClose(), gbc_btnClose);
 
 		GridBagConstraints gbc_profesorForm = new GridBagConstraints();
 		gbc_profesorForm.insets = new Insets(0, 0, 5, 5);
@@ -63,5 +78,17 @@ public class AddChiefJDialog extends JDialog {
 		}
 
 		return profesorForm;
+	}
+	private JButton getBtnClose() {
+		if (btnClose == null) {
+			btnClose = new JButton(Icons.getCloseIcon());
+			btnClose.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					setVisible(false);
+				}
+			});
+			btnClose.setBorder(BorderFactory.createEmptyBorder());
+		}
+		return btnClose;
 	}
 }
